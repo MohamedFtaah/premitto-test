18fe2ca6d06047c0a46e3d526c03ab06cf312d07
3bkr <the39595@gmail.com>
2023-10-24 12:12:05 +0300

    add-react-router-and-tailwind

---
+++ src/Components/Uitily/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className=' mx-auto max-w-2xl  py-6 px-4 sm:px-6 lg:px-8 lg:max-w-7xl flex'>
+            sssads
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/index.js",
+    "./src/Components/Uitily/Navbar.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
