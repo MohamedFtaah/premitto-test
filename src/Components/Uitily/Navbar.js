df6f2cb538d25ad83591e73602af13effe3c8e82
3bkr <the39595@gmail.com>
2023-10-24 14:08:59 +0300

    add-navBar

--- src/Components/Uitily/Navbar.js
+++ src/Components/Uitily/Navbar.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import logo from '../../imgs/1.png'
 export default function Navbar() {
+    const [menu, setMenu] = useState('hidden')
+
     return (
-        <div className=' mx-auto max-w-2xl  py-6 px-4 sm:px-6 lg:px-8 lg:max-w-7xl flex'>
-            sssads
-        </div>
+
+
+
+        <div className=' items-center mx-auto max-w-2xl  py-6 px-4 sm:px-6 lg:px-8 lg:max-w-7xl flex justify-between max-sm:flex-col '>
+            <div className=' flex justify-between max-sm:w-full'>
+                <h1 className=' w-24' ><img src={logo} className=' w-full overflow-hidden rounded-md  lg:aspect-none group-hover:opacity-75 ' ></img></h1>
+                <button onClick={() => { menu === 'hidden' ? setMenu('flex') : setMenu('hidden') }}
+                    className=' w-10 h-10 flex flex-col justify-around items-end hover:scale-105 sm:hidden'>
+                    <span style={{ height: '2px' }} className=' bg-black  w-full '></span>
+                    <span style={{ height: '2px' }} className=' bg-black w-full '></span>
+                    <span style={{ height: '2px' }} className=' bg-black  w-full '></span>
+                </button>
+            </div>
+            <div className={`${menu}  sm:ml-6 sm:flex justify-around w-full max-sm:flex-col`}>
+                <ul className='  items-center flex sm:ml-6 max-sm:flex-col text-center   cursor-pointer'>
+                    <li className='max-sm:py-3 px-6 text-xs hover:font-medium'>Buy</li>
+                    <li className='max-sm:py-3 px-6 text-xs hover:font-medium'>Discover Deals</li>
+                    <li className='max-sm:py-3 px-6 text-xs hover:font-medium'>How it Works</li>
+                    <li className='max-sm:py-3 px-6 text-xs hover:font-medium '>For partners</li>
+                    <li className='max-sm:py-3 px-6 text-xs hover:font-medium'>Blog</li>
+                </ul>
+                <button className=' text-white  text-sm px-3 rounded-md py-2' style={{ backgroundColor: '#861889' }}>Become a partner</button>
+            </div>
+        </div >
     )
 }
